40dac18e0789ddc8f693bbde045f828456759ddc
Ever Santiago Uribe Quintero <everuribe@MacBook-Air-de-Ever.local>
2025-10-05 18:23:13 -0500

    first grade all done.

---
+++ calculators/first_grade.py
@@ -0,0 +1,18 @@
+import math
+
+#form bring by ax + b = c
+
+def first_grade_equation(a, b, c):
+    x = (c - b)/a
+    print(f"the value of x is: {x}")
+
+
+
+
+
+
+
+
+
+
+
---
+++ calculators/main.py
@@ -0,0 +1,2 @@
+from first_grade import first_grade_equation
+
